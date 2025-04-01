f887127dd497a02e4606607454a86ef1e94e2ffc
Raja Balu <rajabalu@gmail.com>
2025-04-02 10:36:16 +1100

    Added language code onstead of globe icon

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -1,34 +1,26 @@
 <template>
-    <v-menu>
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props">
-          {{ getFlag(currentLanguage.value) }} {{ (currentLanguage.value || '').toUpperCase() }}
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item v-for="localeItem in locales" :key="localeItem.code" @click="changeLanguage(localeItem.code)">
-          <v-list-item-title>
-            {{ getFlag(localeItem.code) }} {{ localeItem.name }}
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-  </template>
-  
-  <script setup>
-  import { useI18n } from 'vue-i18n';
-  
-  const { locale, locales, setLocale } = useI18n();
-  
-  const currentLanguage = locale;
-  
-  const getFlag = (code) => {
-    const flags = { en: '🇺🇸', fr: '🇫🇷' };
-    return flags[code] || '🌍';
-  };
-  
-  const changeLanguage = (lang) => {
-    setLocale(lang);
-  };
-  </script>
+  <v-menu>
+    <template v-slot:activator="{ props }">
+      <icon-btn v-bind="props">
+        <v-icon icon="tabler-language" />
+        <span class="ml-1">{{ (locale || '').toUpperCase() }}</span>
+      </icon-btn>
+    </template>
+    <v-list>
+      <v-list-item v-for="localeItem in locales" :key="localeItem.code" @click="changeLanguage(localeItem.code)">
+        <v-list-item-title>{{ localeItem.name }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+
+const { locale, locales, setLocale } = useI18n();
+
+const changeLanguage = (lang) => {
+  setLocale(lang);
+};
+</script>
   
