9c7e485774f57147f5e67392bf889a48f8f3ee44
Raja Balu <rajabalu@gmail.com>
2025-04-05 18:49:42 +1100

    Code cleanup for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,4 @@
 <script setup>
-import OverView from "@/components/dashboard/project/OverView.vue";
-import TasksPerformance from "@/components/dashboard/project/TasksPerformance.vue";
-import ActiveProjects from "@/components/dashboard/project/ActiveProjects.vue";
-import MyTask from "@/components/dashboard/project/MyTask.vue";
-import Teams from "@/components/dashboard/project/Teams.vue";
 const route = useRoute();
 
 const layoutClassic = route.path.includes("horizontal");
@@ -20,29 +15,6 @@ definePageMeta({
 
   <v-container fluid>
     <GlobalsHeader title="Projects" is-btn btn-color="light" btn-text="Create New Project" />
-    <over-view />
-
-    <div class="pb-5">
-      <v-row>
-        <v-col lg="8" cols="12">
-          <active-projects />
-        </v-col>
-        <v-col lg="4" cols="12">
-          <tasks-performance />
-        </v-col>
-      </v-row>
-    </div>
-
-    <div class="pb-5">
-      <v-row>
-        <v-col lg="6" cols="12">
-          <my-task />
-        </v-col>
-        <v-col lg="6" cols="12">
-          <teams />
-        </v-col>
-      </v-row>
-    </div>
   </v-container>
 </template>
 
